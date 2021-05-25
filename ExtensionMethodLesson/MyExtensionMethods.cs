86930ba18b5ec86da875e00a0bd925d2d4d6ec2f
Mattea Swain <mmswain@tql.com>
2021-05-25 12:32:28 -0400

    Add project files.

---
+++ ExtensionMethodLesson/MyExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodLesson
+{
+   static class MyExtensionMethods
+    {
+
+        public static string ToUpperCare(this string s)
+        {
+            return s.ToUpper();
+        }
+        public static void ToConsole(this string str)
+        {
+            Console.WriteLine($"Who runs the world? {str}");
+        }
+
+    }
+}
---
+++ ExtensionMethodLesson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethodLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Mia = "Mia!";
+            Mia.ToUpper().ToConsole();
+        }
+    }
+}
